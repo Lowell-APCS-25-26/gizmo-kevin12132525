5f63376f516e67e67cb48e77846476b2ef4acda0
kevin12132525 <kevin@andrew-gordon.com>
2026-04-09 05:15:31 +0000

    commit 1a2b3c4d5e6f7g8h9i0j

--- lib/src/main/java/org/APCSLowell/OnlinePurchaseManager.java
+++ lib/src/main/java/org/APCSLowell/OnlinePurchaseManager.java
@@ -14,9 +14,11 @@ public class OnlinePurchaseManager {
         /* to be implemented in part (a) */
         int count = 0;
         for(int i = 0; i < purchases.size(); i++){
-            if(purchases.get(i).isElectronic()  && purchases.get(i).getMaker().equals(maker))
+            if(purchases.get(i).isElectronic()  && purchases.get(i).getMaker().equals(maker)) {
+                count++;
+            }
         }
-
+        return count;
     }
 
     /** Returns true if any pair of adjacent purchased Gizmo objects are
@@ -24,7 +26,12 @@ public class OnlinePurchaseManager {
      */
     public boolean hasAdjacentEqualPair() {
         /* to be implemented in part (b) */
-
+        for(int i = 0; i < purchases.size() - 1; i++){
+            if(purchases.get(i).equals(purchases.get(i+1))){
+                return true;
+            }
+        }
+        return false;
     }
     public OnlinePurchaseManager() {
         purchases = new ArrayList <Gizmo>();
